1458b86e11c6067f6f0fd0288128b0c1972fb98e
khaledsawan <khaled963sawan@gmail.com>
2024-06-10 20:10:10 +0300

    update

--- transaction-service/src/main/java/com/hexagon/transactionservice/controller/TransactionController.java
+++ transaction-service/src/main/java/com/hexagon/transactionservice/controller/TransactionController.java
@@ -3,19 +3,30 @@ package com.hexagon.transactionservice.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpServerErrorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.hexagon.transactionservice.model.Transaction;
 import com.hexagon.transactionservice.repository.TransactionRepository;
+import com.hexagon.transactionservice.dto.CustomerResponse;
 
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
     private final TransactionRepository repository;
+    private final RestTemplate restTemplate;
 
-    public TransactionController(TransactionRepository repository) {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionController.class);
+
+    @Autowired
+    public TransactionController(TransactionRepository repository, RestTemplate restTemplate) {
         this.repository = repository;
+        this.restTemplate = restTemplate;
     }
 
     @PostMapping
@@ -23,7 +34,7 @@ public class TransactionController {
         return repository.save(transaction);
     }
     @GetMapping("/{id}")
-    public Optional<Transaction> fetchTransactionById(@PathVariable int id){
+    public Optional<Transaction> fetchTransactionById(@PathVariable int id) {
         return repository.findById(id);
     }
 
@@ -41,5 +52,16 @@ public class TransactionController {
     @GetMapping("/buyer/{buyerId}")
     public List<Transaction> getTransactionsByBuyer(@PathVariable String buyerId) {
         return repository.findByBuyerId(buyerId);
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public CustomerResponse getCustomerDetails(@PathVariable int customerId) {
+        String customerServiceUrl = "http://CUSTOMER-SERVICE/customer/" + customerId;
+        try {
+            return restTemplate.getForObject(customerServiceUrl, CustomerResponse.class);
+        } catch (HttpServerErrorException e) {
+            logger.error("Error calling CUSTOMER-SERVICE: {}", e.getMessage());
+            throw e;
+        }
     }
 }
